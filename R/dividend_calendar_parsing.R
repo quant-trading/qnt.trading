c58787e051f3bcf496ada1e5e2e00b0b1d350e2b
MikeAndr <qnt.trading@gmail.com>
2016-09-14 11:36:03 -0400

    parsing dividends 02

--- R/dividend_calendar_parsing.R
+++ R/dividend_calendar_parsing.R
@@ -1,33 +1,58 @@
+
+#install.packages('XML')
+#install.packages('RPostgreSQL')
+
 y = tryCatch({
-
   
-library(XML)
-#y <- htmlParse("http://bcs-express.ru/dividednyj-kalendar")
-y <- htmlParse("http://bcs-express.ru/kalendari-investora/dividends/2015")
-
-#recs = getNodeSet(y, "//*[@id='con_tab3']//*[@class='calendar_item_header clearfix']")
-recs = getNodeSet(y, "//*[@id='con_tab3']//*[@class='clearfix']")
-
-#company <- sapply(recs, xpathSApply, ".//*[@class='divids_item_name item_header2']", xmlValue)
-#div_size <- sapply(recs, xpathSApply, ".//*[@class='divids_item_size item_content2']", xmlValue)
-company <- sapply(recs, xpathSApply, ".//*[@class='divids_item_name item_header']", xmlValue)
-div_size <- sapply(recs, xpathSApply, ".//*[@class='divids_item_size item_content']", xmlValue)
-
-
-stock_price <- sapply(recs, xpathSApply, ".//*[@class='divids_item_price']", xmlValue)
-#return_rate <- sapply(sapply(recs, xpathSApply, ".//*[@class='divids_item_dohod item_content2']", xmlValue), function(k) { as.numeric(gsub(" ","",gsub(",",".",k)))})
-#last_trading_date <- as.Date(sapply(recs, xpathSApply, ".//*[@class='divids_item_sobran item_content2']", xmlValue), "%d.%m.%Y")
-#close_registry_date <- as.Date(sapply(recs, xpathSApply, ".//*[@class='divids_item_zakritie item_content2']", xmlValue), "%d.%m.%Y")
-
-#return_rate <- sapply(sapply(recs, xpathSApply, ".//*[@class='divids_item_dohod item_content2']", xmlValue), function(k) { as.numeric(gsub(" ","",gsub(",",".",k)))})
-#last_trading_date <- as.Date(sapply(recs, xpathSApply, ".//*[@class='divids_item_sobran item_content2']", xmlValue), "%d.%m.%Y")
-#close_registry_date <- as.Date(sapply(recs, xpathSApply, ".//*[@class='divids_item_zakritie item_content2']", xmlValue), "%d.%m.%Y")
-
-return_rate <- sapply(sapply(recs, xpathSApply, ".//*[@class='divids_item_dohod item_content']", xmlValue), function(k) { as.numeric(gsub(" ","",gsub(",",".",k)))})
-last_trading_date <- as.Date(sapply(recs, xpathSApply, ".//*[@class='divids_item_sobran item_content']", xmlValue), "%d.%m.%Y")
-close_registry_date <- as.Date(sapply(recs, xpathSApply, ".//*[@class='divids_item_zakritie item_content']", xmlValue), "%d.%m.%Y")
-
-
-data = cbind(company, div_size, stock_price, return_rate, last_trading_date, close_registry_date)
-
+  require(XML)
+  
+  Sys.setlocale("LC_ALL", 'russian')
+  
+    #y <- htmlParse(paste("http://bcs-express.ru/kalendari-investora/dividends/",i),encoding="utf-8")
+    y <- htmlParse("http://bcs-express.ru/dividednyj-kalendar")
+    recs = getNodeSet(y, "//*[@id='con_tab3']//*[@class='calendar_item_header clearfix']")
+    
+    company <- sapply(recs, xpathSApply, ".//*[@class='divids_item_name item_header2']", xmlValue)
+    div_size <- sapply(recs, xpathSApply, ".//*[@class='divids_item_size item_content2']", xmlValue)
+    
+    
+    stock_price <- sapply(recs, xpathSApply, ".//*[@class='divids_item_price']", xmlValue)
+    return_rate <- sapply(sapply(recs, xpathSApply, ".//*[@class='divids_item_dohod item_content2']", xmlValue), function(k) { as.numeric(gsub(" ","",gsub(",",".",k)))})
+    last_trading_date <- sapply(recs, xpathSApply, ".//*[@class='divids_item_sobran item_content2']", xmlValue)
+    close_registry_date <- sapply(recs, xpathSApply, ".//*[@class='divids_item_zakritie item_content2']", xmlValue)
+    upload_dt = rep(Sys.Date(),length(company))
+    data = data.frame(cbind(upload_dt, company, div_size, stock_price, return_rate, last_trading_date, close_registry_date))
+    
+    data$upload_dt = rep(Sys.Date(),length(company))
+    data$last_trading_date = as.Date(data$last_trading_date, "%d.%m.%Y")
+    data$close_registry_date = as.Date(data$close_registry_date, "%d.%m.%Y")
+    
+    
+    names(data) = c("upload_dt","div_event","div_size","stock_price","return_rate","last_trading_date","close_registry_date")
+    
+    # DB routine
+    require("RPostgreSQL")
+    
+    # create a connection
+    # save the password that we can "hide" it as best as we can by collapsing it
+    pw <- {
+      "kalinovmost19842006"
+    }
+    
+    # loads the PostgreSQL driver
+    drv <- dbDriver("PostgreSQL")
+    # creates a connection to the postgres database
+    # note that "con" will be used later in each connection to the database
+    con <- dbConnect(drv, dbname = "postgres",
+                     host = "localhost", port = 5432,
+                     user = "postgres", password = pw)
+    rm(pw) # removes the password
+    
+    
+    dbWriteTable(con, "dividends_bks", value = data, append = TRUE, row.names = FALSE)
+    
+    
+    # close the connection
+    dbDisconnect(con)
+    dbUnloadDriver(drv)
 })
