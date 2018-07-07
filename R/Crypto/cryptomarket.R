ef75f898886d6dc677eedd8a5535a155bfc52bcb
quant-trading <qnt.trading@gmail.com>
2018-07-06 20:19:53 -0700

    Cryptocurrency marketcap
    coinmarketcapr package initial script

---
+++ R/Crypto/cryptomarket.R
@@ -0,0 +1,7 @@
+library(coinmarketcapr)
+
+global_cap <- get_global_marketcap(currency = "USD")
+coins_data <- get_marketcap_ticker_all(currency = "USD")
+
+coins_data[which(coins_data$percent_change_7d == max(coins_data$percent_change_7d)),c("symbol", "percent_change_7d")]
+coins_data[which(coins_data$percent_change_7d == min(coins_data$percent_change_7d)),c("symbol", "percent_change_7d")]
